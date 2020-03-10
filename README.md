8b182ebb5691ec356c995cff74fa686af7a6ecf7
etcroot <trashbxg@gmail.com>
2020-03-10 09:44:49 +0100

    replacing with new source

--- README.md
+++
@@ -1,5 +0,0 @@
-![Kitsune](https://raw.githubusercontent.com/trashbxg/Ecchi/master/assets/img/kitsunegithub.png)
-
-# Coming Soon!
-
-## Don't expect anything to work as intended atm, i'm just uploading my daily progresses.
--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js');
-
-exports.run = (client, message, args) => {
-    const embed = new Discord.MessageEmbed()
-    .setDescription(`**Pong!** - Ping: \`${Math.round(client.ws.ping)}ms\``)
-    .setColor(client.color)
-    return message.channel.send(embed);
-
-}
